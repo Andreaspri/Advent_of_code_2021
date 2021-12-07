fdfe648e03bd920c2e2d81c385cd5d23e31348f5
Andreas Primstad <andreaspr@uia.no>
2021-12-07 07:19:15 +0100

    Day 7 done!

---
+++ day_7/main.py
@@ -0,0 +1,33 @@
+
+
+def part_1():
+    with open("data.txt") as f:
+        data = [int(i) for i in f.read().split(",")]
+
+    fuel_consumption = {}
+
+    for i in range(min(data),max(data)):
+        fuel_consumption[i] = sum([crab-i if crab > i else i-crab for crab in data])
+
+
+    return min(fuel_consumption.values())
+
+
+def part_2():
+    with open("data.txt") as f:
+        data = [int(i) for i in f.read().split(",")]
+
+    fuel_consumption = {}
+
+    for i in range(min(data), max(data)):
+        fuel_consumption[i] = sum([abs((crab-i))*(abs(crab-i) + 1)/2 if crab > i else abs((crab-i))*(abs(crab-i) + 1)/2 for crab in data])
+
+
+    return min(fuel_consumption.values())
+
+
+if __name__ == '__main__':
+    print(part_1())
+    print(part_2())
+
+
