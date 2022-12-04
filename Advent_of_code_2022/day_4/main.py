5bd9677c46d25e83e29cbefc32642086a43835ab
Andreas Primstad <andreaspr@uia.no>
2022-12-04 15:04:40 +0100

    Day 4 complete

---
+++ Advent_of_code_2022/day_4/main.py
@@ -0,0 +1,42 @@
+import os
+
+
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+
+def part_1(data):
+    count = 0
+    for line in data:
+        first, second = line.split(",")
+        first_set  = set(range(int(first.split("-")[0]), int(first.split("-")[1]) + 1))
+        second_set = set(range(int(second.split("-")[0]), int(second.split("-")[1]) + 1))
+        if first_set.issubset(second_set):
+            count += 1
+        elif second_set.issubset(first_set):
+            count += 1
+
+    return count
+
+
+
+def part_2(data):
+    count = 0
+    for line in data:
+        first, second = line.split(",")
+        first_range = set(range(int(first.split("-")[0]), int(first.split("-")[1]) + 1))
+        second_range = set(range(int(second.split("-")[0]), int(second.split("-")[1]) + 1))
+        if first_range & second_range:
+            count += 1
+
+    return count
+
+
+
+if __name__ == "__main__":
+    with open("data.txt") as f:
+        data = f.read().split("\n")
+
+    print("Part 1:", part_1(data))
+    print("Part 2:", part_2(data))
